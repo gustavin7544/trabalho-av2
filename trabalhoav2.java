57a4af8ec86f0271e7c949728486649089fe76a7
gustavin7544 <135707284+gustavin7544@users.noreply.github.com>
2024-12-02 09:35:09 -0300

    Add files via upload

---
+++ trabalhoav2.java
@@ -0,0 +1,69 @@
+      package entidades;
+       
+       public class produto{
+        
+         private int codigo;
+         private string nome;
+         private int estoque
+         private double valorunitario
+         
+         punlic produto( int codigo; string nome) {
+            
+            this.codigo=codigo;this.nome=nome;
+            this.nome=nome;
+
+            }
+            
+            public int getcodigo(){
+                return codigo;
+
+
+                public string getnome() {
+            return nome;
+
+                }
+
+                public.void setnome(string nome) {
+                    this.nome=nome;
+
+
+
+             public void setnome(string nome) {
+                this.nome= nome;
+
+
+                public int getestoque() {
+                    return estoque;
+
+                    public void setcodigo( int codigo)
+                    this.codigo= codigo;
+
+
+                    public double getvalounitario (()) {
+                        this.valorunitario= valorunitario;
+
+
+                        \\metodos
+                        public void incluirEstoque (int quantidade)
+                         estoque= estoque + quantidade;
+
+                    }
+                    public voi tirar =Estoque(int qyantidade) }
+                    estoque= estoque- quantidade;
+
+
+
+                    if (quantidade) <= estoque)
+                                    estoque = estoque - quantidade;
+             } else  {
+                System.out.println('estoque indisponível!!!');
+
+             }
+
+
+
+                }
+                
+
+
+
